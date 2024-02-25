b40d31830622c230d4d3cdb4a98992de6d9176ea
altiscope2 <107213438+altiscope2@users.noreply.github.com>
2024-02-25 16:22:10 -0600

    Add files via upload

---
+++ coinToss.java
@@ -0,0 +1,17 @@
+package coinToss;
+
+import java.util.Random;
+public class coinToss{
+
+
+public static void heads();
+{ 
+	   System.out.println("Coin landed on heads!");
+
+}
+public static void tails();
+{  
+	   System.out.println("Coin landed on tails!");
+
+}
+}
